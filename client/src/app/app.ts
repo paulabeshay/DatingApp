b8c53411c696d35eb8b8e1e0c8b6a252bbaba47e
Paula Soliman <Paula.Soliman@linkdev.com>
2025-12-25 16:51:05 +0200

    Update

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,12 +1,21 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('client');
+export class App implements OnInit {
+  private readonly http = inject(HttpClient);
+  protected readonly title = signal('Dating app');
+ 
+  ngOnInit(): void {
+    this.http.get("https://localhost:5001/api/members").subscribe({
+      next: members => console.log(members),
+      error: err => console.error(err),
+      complete: () => console.log('Request completed')
+    });
+  }
 }
